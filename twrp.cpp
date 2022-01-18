330550099f020558992d21ec18e7e4dcdde0749d
bigbiff <bigbiff@teamw.in>
2022-01-17 19:31:52 -0500

    fscrypt: wip
    Change-Id: I2cb7385541b8d9c7f1d8de0cef8ed3803b457b8c

--- twrp.cpp
+++ twrp.cpp
@@ -67,6 +67,11 @@ extern "C" {
 }
 #endif
 
+#ifdef TW_INCLUDE_CRYPTO
+#include "FsCrypt.h"
+#include "Decrypt.h"
+#endif
+
 //extern int adb_server_main(int is_daemon, int server_port, int /* reply_fd */);
 
 TWPartitionManager PartitionManager;
@@ -251,8 +256,11 @@ static void process_recovery_mode(twrpAdbBuFifo* adb_bu_fifo, bool skip_decrypti
 	}
 	LOGINFO("Backup of TWRP ramdisk done.\n");
 #endif
-
+#ifdef TW_INCLUDE_CRYPTO
+	android::keystore::copySqliteDb();
+#endif
 	Decrypt_Page(skip_decryption, datamedia);
+
 	// Check for and load custom theme if present
 	TWFunc::check_selinux_support();
 	gui_loadCustomResources();
@@ -350,7 +358,9 @@ static void reboot() {
 	gui_msg(Msg("rebooting=Rebooting..."));
 	TWFunc::Update_Log_File();
 	string Reboot_Arg;
-
+#ifdef TW_INCLUDE_CRYPTO
+	fscrypt_lock_user_key(0);
+#endif
 	DataManager::GetValue("tw_reboot_arg", Reboot_Arg);
 	if (Reboot_Arg == "recovery")
 		TWFunc::tw_reboot(rb_recovery);
